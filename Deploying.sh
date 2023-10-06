ad8f71698dbac06a0612def37d9de49a088a2b31
TejaswiniSruthi <82491774+TejaswiniSruthi@users.noreply.github.com>
2023-10-06 16:05:13 +0530

    Update Deploying.sh

--- Deploying.sh
+++ Deploying.sh
@@ -3,7 +3,7 @@ sudo su
 yum update -y
 yum install -y httpd
 cd /var/www/html
-wge t https://www.tooplate.com/zip-template/2113_earth.zip
+wget https://www.tooplate.com/zip-template/2113_earth.zip
 unzip 2113_earth.zip
 cp -r 2113_earth/* /var/www/html/
 systemctl start httpd
